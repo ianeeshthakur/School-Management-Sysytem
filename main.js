6a9c9083759e0f5fb5c1771b76c7a071f8e2f3a1
Aneesh Thakur <aneeshthakur134@gmail.com>
2025-09-04 09:32:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+let icon=document.querySelector("#text");
+
+let select=document.querySelector("#elect");
+
+let options=document.querySelector("#appear");
+
+let btn=document.querySelector(".btn")
+
+let staff=document.querySelector("#staff"); // staff icon par click then staff page open
+
+let small=document.querySelector("small");
+
+let form=document.querySelector("form");
+
+let loginbox=document.querySelector("#loginbox");
+
+let user=document.querySelector("#user");
+
+let password=document.querySelector("#password");
+
+let student=document.querySelector("#student");
+
+let calendar=document.querySelector("#calendar");
+
+let calu=document.querySelector(".cal");
+
+let cal=document.querySelector("#cal");
+
+calendar.addEventListener("click",function(){
+
+});
+
+
+staff.addEventListener("click",function(){
+    window.location.href = "staff.html";
+});
+
+
+
+student.addEventListener("click",function(){
+    window.location.href="student.html";
+});
+
+calendar.addEventListener("click",function(){
+    window.location.href="calendar.html";
+});
+
+luet.addEventListener("click",function(){
+    window.location.href="main.html";                                             
+})
+
+cal.addEventListener("dblclick",function(){
+  window.location.href="fke3bfk.html";
+})
